475880fd96d73b800c384e3112db73d23f90bee7
freesix <17781444956@163.com>
2023-11-08 19:04:23 +0800

    add

---
+++ 3DMax-Cliques/Linux/build/CMakeFiles/MAC.dir/DependInfo.cmake
@@ -0,0 +1,24 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/freesix/3DMax-Cliques/Linux/PCR.cpp" "CMakeFiles/MAC.dir/PCR.cpp.o" "gcc" "CMakeFiles/MAC.dir/PCR.cpp.o.d"
+  "/home/freesix/3DMax-Cliques/Linux/desc_dec.cpp" "CMakeFiles/MAC.dir/desc_dec.cpp.o" "gcc" "CMakeFiles/MAC.dir/desc_dec.cpp.o.d"
+  "/home/freesix/3DMax-Cliques/Linux/funcs.cpp" "CMakeFiles/MAC.dir/funcs.cpp.o" "gcc" "CMakeFiles/MAC.dir/funcs.cpp.o.d"
+  "/home/freesix/3DMax-Cliques/Linux/main.cpp" "CMakeFiles/MAC.dir/main.cpp.o" "gcc" "CMakeFiles/MAC.dir/main.cpp.o.d"
+  "/home/freesix/3DMax-Cliques/Linux/registration.cpp" "CMakeFiles/MAC.dir/registration.cpp.o" "gcc" "CMakeFiles/MAC.dir/registration.cpp.o.d"
+  "/home/freesix/3DMax-Cliques/Linux/visualization.cpp" "CMakeFiles/MAC.dir/visualization.cpp.o" "gcc" "CMakeFiles/MAC.dir/visualization.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- python_test/test.py
+++ python_test/test.py
@@ -1,2 +1,9 @@
-import os 
-i
+str1 = "Runoobexample....wow!!!"
+str2 = "exam"
+print(str1.find(str2,8))
+# def change_list(list1):
+#     list1.append('newStr')
+# if __name__ == '__main__':
+#     list1 = [1,2,3]
+#     change_list(list1)
+#     print(list1)
